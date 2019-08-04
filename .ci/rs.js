550403f60155480a521d6241af0d7d623f25be84
Parham Alvani <parham.alvani@gmail.com>
2019-08-05 02:05:13 +0430

    feat: Add Replication Initiation Method

---
+++ .ci/rs.js
@@ -0,0 +1,15 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 05-08-2019
+ * |
+ * | File Name:     rs.js
+ * +===============================================
+ */
+rs.initiate()
+rs.add("m2:27017")
+rs.add("m2:27017")
